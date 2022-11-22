a5f3d0916511f1d36318d5d045e9bf8bac321dd8
IVANLACSON\Admin <ibanlacson@gmail.com>
2022-11-22 16:20:14 +0800

    intent doesn't work

--- app/src/main/java/com/auf/cea/openweatherapilesson/MainMenuActivity.kt
+++ app/src/main/java/com/auf/cea/openweatherapilesson/MainMenuActivity.kt
@@ -3,6 +3,7 @@ package com.auf.cea.openweatherapilesson
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.auf.cea.openweatherapilesson.databinding.ActivityMainMenuBinding
 
@@ -20,15 +21,17 @@ class MainMenuActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(p0: View?) {
         when(p0!!.id) {
             (R.id.btn_five_day) -> {
-                val intent = Intent(this@MainMenuActivity,MainActivity::class.java)
-                intent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                 val intent = Intent(this@MainMenuActivity,MainActivity::class.java)
+//                intent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(intent)
+                Log.d(MainMenuActivity::class.java.simpleName, "Napindot yung five day")
 
             }
             (R.id.btn_search_city) -> {
-                val intent = Intent(this@MainMenuActivity,MainMenuActivity::class.java)
-                intent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                 val intent = Intent(this@MainMenuActivity,SearchCityActivity::class.java)
+//                intent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(intent)
+                Log.d(MainMenuActivity::class.java.simpleName, "Napindot yung current")
             }
         }
     }
